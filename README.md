ed67286cb95c786dcbd3596b90262a2b730e6634
Haley Halsey <haleyhalsey@yahoo.com>
2020-12-24 12:16:56 -0800

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Weather_App
+
+At this point I am submitting this as a place holder and will resubmit soon. 
---
+++ assets/script.js
@@ -0,0 +1,11 @@
+var get5Day = function(city){
+    var apiKey = "a1c46c139d448aef037f120b234bec2d"
+    var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
+
+    fetch(apiURL)
+    .then(function(response){
+        response.json().then(function(data){
+           display5Day(data);
+        });
+    });
+};
